800ebc8f65211a21aaafcb93aa2065b80a334983
100835 <112314721+100835@users.noreply.github.com>
2025-01-18 17:01:17 +0800

    测试了map和键值对<string,int>的使用，以及字符串的拼接操作

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
+void printMap(map<string,int> &);
+
 int main()
 {
-    int a = 10;
-    int b = 20;
-//    int* const p = &a;
-    const int* p = &a;
-    p = &b;
-    cout << "*p = " << *p << endl;
+    string s;
+    map<string,int> m;
+    cin >> s;
+    while(s != "0")
+    {
+        m[s]++;
+        cin >> s;
+    }
+    printMap(m);
 
     return 0;
+}
+
+void printMap(map<string,int> &m)
+{
+    for(auto & it : m)
+        cout << "key = " << it.first << " value = " << it.second << endl;
 }
