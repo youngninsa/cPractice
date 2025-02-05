c9b77d1fea7bc9e3c455f13c7068212f01fba0e1
100835 <112314721+100835@users.noreply.github.com>
2025-02-05 15:56:00 +0800

    能量项链（区间dp）

--- src/luogu/p1063.cpp
+++ src/luogu/p1063.cpp
@@ -12,13 +12,16 @@ int main()
         cin >> e[i];
         e[n+i] = e[i];
     }
-    for(int r=2;r<2*n;r++)
+    for(int r=2;r<2*n;r++)  //区间右边界
     {
-        for(int l=r-1;l>=1 && r-l<n;l--)
+        for(int l=r-1;l>=1 && r-l<n;l--)    //区间左边界
         {
-            for(int mid=l;mid<r;mid++)
+            for(int mid=l;mid<r;mid++)  //划分左右区间的划分
                 f[l][r] = max(f[l][r],f[l][mid]+f[mid+1][r]+e[l]*e[mid+1]*e[r+1]);
-            if(ans < f[l][r])
+            /**
+             * 状态方程: 该区间能量 = max(不操作的能量，左区间的能量+右区间的能量+合并后的能量)
+             */
+            if(ans < f[l][r])   //找到最大能量
                 ans = f[l][r];
         }
     }
