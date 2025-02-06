6b5b5554c6888c5b69da0ab03b521e339b9c26f4
100835 <112314721+100835@users.noreply.github.com>
2025-02-06 23:37:57 +0800

    测试了set容器的使用

--- src/test/STL.cpp
+++ src/test/STL.cpp
@@ -1,35 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<bits/stdc++.h>
 
 using namespace std;
 
-void print(int n) {
-    cout << n << endl;
-}
-
 int main()
 {
-    vector<int> vec,v;
-
-    vec.push_back(10);
-    vec.push_back(20);
-    v.push_back(30);
-    v.push_back(40);
-
-
-    //for_each(vec.begin(),vec.end(), print);
-    for(int i = 0;i < 100000;i++)
-        vec.push_back(i);
-    for(int i =0 ;i < 3;i++)
-        v.push_back(i);
-
-    cout << vec.capacity() << endl;
-    cout << v.capacity() << endl;
-
-    vector<int> (v).swap(vec);
-
-    cout << vec.capacity() << endl;
-    cout << v.capacity() << endl;
+    set<int> s;
+    int n,a[1005]={0};
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        cin >> a[i];
+        s.insert(a[i]);
+    }
+    cout << s.size() << endl;
+    for(auto & it:s)
+        cout << it << " ";
     return 0;
 }
