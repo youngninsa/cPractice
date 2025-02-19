de47c1b27a9f04903178955969f88c70ff77f64c
100835 <112314721+100835@users.noreply.github.com>
2025-02-19 17:03:38 +0800

    优化了牛客周赛题的解法

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,20 +1,29 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector v(n+1,vector<int>(1));
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=i;j++)
+            cin >> v[i][j];
+    for(int i=1;i<n;i++)
+        for(int j=1;j<=i;j++)
+            if (v[i][j]>v[i+1][j+1] && v[i][j]>v[i+1][j])
+            {
+                cout<<"No"<<endl;
+                return;
+            }
+    cout<<"Yes"<<endl;
+}
 int main()
 {
-    int n,temp,ma=0;
-    map<int,int> m;
-    cin >> n;
-    for(int i=0;i<n;i++)
-    {
-        cin >> temp;
-        m[temp]++;
-    }
-    for(auto &it:m)
-        ma = max(ma,it.second);
-    cout << ma;
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
     return 0;
 }
