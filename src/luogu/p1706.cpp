98456a5a739e85fdf2060bfac9b2b0f35d5bdf6f
100835 <112314721+100835@users.noreply.github.com>
2025-02-21 00:20:41 +0800

    字典序全排列（dfs）

--- src/luogu/p1706.cpp
+++ src/luogu/p1706.cpp
@@ -3,18 +3,35 @@
 using namespace std;
 using ll = long long;
 
+int n,a[10]={0},b[10]={0};
+bool used[10]={false};
+
+void dfs(int k)
+{
+    if(k>n)
+    {
+        for(int i=1;i<=n;i++)
+            cout << setw(5) << b[i];
+        cout << endl;
+        return ;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        if(!used[i])
+        {
+            b[k]=a[i];
+            used[i]=true;
+            dfs(k+1);
+            used[i]=false;
+        }
+    }
+}
 
 int main()
 {
-    int n,a[10]={0};
     cin >> n;
-    for(int i=0;i<n;i++)
-        a[i]=i+1;
-    do
-    {
-        for(int i=0;i<n;i++)
-            printf("%5d",a[i]);
-        cout << endl;
-    }while(next_permutation(a,a+n));
+    for(int i=1;i<=n;i++)
+        a[i]=i;
+    dfs(1);
     return 0;
 }
