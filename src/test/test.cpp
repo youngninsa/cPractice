0c459dcd6ceee459b988efbc865e01b97edd90dd
100835 <112314721+100835@users.noreply.github.com>
2025-03-07 16:39:43 +0800

    测试

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -2,28 +2,17 @@
 
 using namespace std;
 
-void solve()
+class speak
 {
-    int n;
-    cin >> n;
-    vector v(n+1,vector<int>(1));
-    for(int i=1;i<=n;i++)
-        for(int j=1;j<=i;j++)
-            cin >> v[i][j];
-    for(int i=1;i<n;i++)
-        for(int j=1;j<=i;j++)
-            if (v[i][j]>v[i+1][j+1] && v[i][j]>v[i+1][j])
-            {
-                cout<<"No"<<endl;
-                return;
-            }
-    cout<<"Yes"<<endl;
-}
+public:
+    speak()
+    {
+        cout << "hello java.cpp";
+    }
+};
+
 int main()
 {
-    int t;
-    cin >> t;
-    while(t--)
-        solve();
+    speak sp=speak();
     return 0;
 }
