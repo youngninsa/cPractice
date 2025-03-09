17ecc00e544875abf1fe41be998274a9ab31ced3
100835 <112314721+100835@users.noreply.github.com>
2025-03-09 16:41:01 +0800

    .

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,18 +1,63 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-class speak
+int sum(const int *a,int step)
 {
-public:
-    speak()
-    {
-        cout << "hello java.cpp";
-    }
-};
+    int ans=0;
+    for (int i=0; i < sizeof(a)/sizeof(int); i+=step)
+        ans += *(a+i);
+}
+
+double sum(const double *a,int step)
+{
+    double ans=0;
+    for (int i=0; i < sizeof(a)/sizeof(int); i+=step)
+        ans += *(a+i);
+}
+
+//void solve3()
+//{
+//#define c2(n) n*(n-1)/2
+//    cout << c2(5);
+//}
+
+//void solve4()
+//{
+//    srand(static_cast<unsigned long long> (time(nullptr)) * 38646925674 % 12345);
+//    int num = rand()%100+1;
+//    int guess;
+//    cout << num;
+//    /*while(guess != num)
+//    {
+//        cout << "please enter your guess number:";
+//        cin >> guess;
+//        if(guess > num)
+//            cout << "bigger" << endl;
+//        if(guess < num)
+//            cout << "smaller" << endl;
+//    }*/
+//    do
+//    {
+//        cout << "please enter your guess number:";
+//        cin >> guess;
+//        if(guess > num)
+//            cout << "bigger" << endl;
+//        if(guess < num)
+//            cout << "smaller" << endl;
+//    }while(guess != num);
+//    cout << "congratulations!";
+//}
+
+void solve5()
+{
+    int num1[]={1,2,3,4,5,6,7,8,9};
+    double num2[]={1.1,2.2,3.3,4.4,5.5,6.6,7.7,8.8,9.9};
+    cout << sum(num1,1) << endl;
+    cout << sum(num2,1);
+}
 
 int main()
 {
-    speak sp=speak();
+    solve5();
     return 0;
 }
