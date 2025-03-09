39d464b7ee33de22fd8e6a516ec779cc4cd6a863
100835 <112314721+100835@users.noreply.github.com>
2025-03-09 16:39:40 +0800

    测试了文件输出

--- src/test/outfile.cpp
+++ src/test/outfile.cpp
@@ -9,9 +9,9 @@ int main()
 
     file.open("test.txt",ios::out);
 
-    file << "name:Alex" << endl;
-    file << "gender:male" << endl;
-    file << "age:19" << endl;
+    file << "r  v  100000123" << endl;
+    file << "h  h  100000147" << endl;
+    file << "c  c  100000348" << endl;
 
     file.close();
     return 0;
