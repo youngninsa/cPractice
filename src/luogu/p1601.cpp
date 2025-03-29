14a7cb18d37eb162912196e5620f487d3ec42016
100835 <112314721+100835@users.noreply.github.com>
2025-03-29 16:12:00 +0800

    高精度加法

--- src/luogu/p1601.cpp
+++ src/luogu/p1601.cpp
@@ -2,26 +2,27 @@
 
 using namespace std;
 using ll = long long;
+using ull = unsigned long long;
 
 string add(string str1,string str2)
 {
     string str;
-    int len1=str1.length();
-    int len2=str2.length();
+    ull len1=str1.length();
+    ull len2=str2.length();
     if(len1 > len2)
         for(int i=1;i<=len1-len2;i++)
-            str2 = "0"+str2;
+            str2 += "0";
     else
         for(int i=1;i<=len2-len1;i++)
-            str1 = "0"+str1;
-    int len = str1.length();
+            str1 += "0";
+    ull len = str1.length();
     int sum=0,temp=0;
-    for(int i=len-1;i>=0;i--)
+    for(ll i=len-1;i>=0;i--)
     {
         sum = str1[i]-'0'+str2[i]-'0'+temp;
         temp = sum/10;
         sum %= 10;
-        str = char(sum+'0') + str;
+        str += char(sum+'0');
     }
     if(temp!=0)
         str = char(temp + '0') + str;
