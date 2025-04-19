c48c9f64f3f2afd9d2dc180fe70b8dd9b48d25ec
youngninsa <youngninsa@gmail.com>
2025-04-19 17:18:16 +0800

    更新了解法,以及四舍五入lround()库函数的使用

--- src/luogu/P12130.cpp
+++ src/luogu/P12130.cpp
@@ -8,7 +8,7 @@ int main()
     double l,r = sqrt(pow(233,2)+ pow(666,2));
     int ans;
     l= atan(666.0/233)*r;
-    ans = (int)(r+l+0.5);
+    ans = lround(r+l);
     cout << ans;
     return 0;
 }
