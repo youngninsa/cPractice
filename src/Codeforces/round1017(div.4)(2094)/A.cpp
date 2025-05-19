6bf8611b7fbc2c89377b7f148a510ca13f0024da
youngninsa <youngninsa@gmail.com>
2025-05-19 19:39:25 +0800

    第1017场A题

---
+++ src/Codeforces/round1017(div.4)(2094)/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        for(int i=0;i<3;i++)
+        {
+            string tem;
+            cin >> tem;
+            cout << tem[0];
+        }
+        cout << endl;
+    }
+    return 0;
+}
