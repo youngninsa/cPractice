0b107e1ec8bcc0423c18c948cf00d2bc4328d806
youngninsa <youngninsa@gmail.com>
2025-05-30 19:52:48 +0800

    改进了拓扑排序

--- src/test/topological_sorting.cpp
+++ src/test/topological_sorting.cpp
@@ -6,63 +6,61 @@ const int N = 1e2+9;
 
 class TopoSort{
 public:
-    int n=0,m=0,ind[N]={0},outd[N]{0};
+    int n=0,m=0,ind[N]={0};
     vector<int> G[N],ans;
     queue<int> q;
     void creatG();
-    bool isDAG();
+    void sort();
 };
 
 void TopoSort::creatG()
 {
-    cout << "please input the n and the m:";
     cin >> this->n >> this->m;
     for(int i=1;i<=this->m;i++)
     {
         int x,y;
-        cout << "please input the edge:";
         cin >> x >> y;
-        this->outd[x]++;this->ind[y]++;
+        if(x==y)
+            throw runtime_error("cycle detected");
+        this->ind[y]++;
         this->G[x].push_back(y);
     }
         for(int i=1;i<=this->n;i++)
         if(this->ind[i]==0)
             this->q.push(i);
 }
-bool TopoSort::isDAG()
+void TopoSort::sort()
 {
-    try
-    {
         while(!this->q.empty())
         {
             int x = this->q.front();
             this->ans.push_back(x);
             this->q.pop();
-            for(auto i:this->G[x])
+            for(auto &i:this->G[x])
             {
+
                 this->ind[i]--;
                 if(this->ind[i] == 0)
                     this->q.push(i);
             }
         }
         if(this->ans.size() != this->n)
-            throw false;
-    }
-    catch (bool &rs)
-    {
-        if(!rs)
-            cout << "ERROR" << endl << "¸ÃÍ¼ÓÐ»·";
-        return false;
-    }
-    return true;
+            throw runtime_error("cycle detected");
 }
 
 int main()
 {
-    TopoSort t;
-    t.creatG();
-    if(t.isDAG())
+    try {
+        TopoSort t;
+        t.creatG();
+        t.sort();
         for(auto i:t.ans)
             cout << i << " ";
+    }
+    catch (const runtime_error &e)
+    {
+        cout << e.what() << endl;
+    }
+
     return 0;
 }
